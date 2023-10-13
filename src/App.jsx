61a87af7d1b7320618eda4c1f322bf4959530046
kethesainikhil <nikhilkethe007@gmail.com>
2023-10-13 17:53:24 +0530

    added reactrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './pages/Home'
-import Werhiring from './pages/werhiring'
+import Werhiring from './pages/Werhiring'
 import { Routes , Route } from 'react-router-dom'
 const App = () => {
   return (
